364ea66a1ce99252023d614f09392c388b92a2d5
deannilson <deannilson43@gmail.com>
2025-06-01 05:58:14 +0100

    modified:   .nuxt/dev/index.mjs 	modified:   .nuxt/manifest/latest.json 	modified:   .nuxt/manifest/meta/dev.json 	modified:   .nuxt/nitro.json 	modified:   .nuxt/tailwind.config.cjs 	modified:   components/vHeader.vue 	modified:   node_modules/.cache/jiti/utils-index.148aa3a7.mjs

--- components/vHeader.vue
+++ components/vHeader.vue
@@ -29,18 +29,40 @@
             <img src="/logo.jpg" width="160" />
           </nuxt-link>
           <div class="flex-1 flex justify-end items-center gap-8">
-            <nuxt-link
-              v-for="n in links"
-              :to="$nav({ name: n.name })"
-              class="py-4 border-y-4 border-y-transparent"
-              :class="
-                route.name.startsWith(n.name)
-                  ? 'border-b-primary text-primary font-bold'
-                  : 'hover:border-b-primary hover:text-primary font-medium'
-              "
+            <div
+              v-for="(n, i) in links"
+              class="relative group h-24 inline-flex items-center"
             >
-              {{ n.title }}
-            </nuxt-link>
+              <nuxt-link
+                :to="$nav({ name: n.name })"
+                class="py-4 border-y-4 border-y-transparent"
+                :class="
+                  route.name.startsWith(n.name)
+                    ? 'border-b-primary text-primary font-bold'
+                    : 'hover:border-b-primary hover:text-primary font-medium'
+                "
+              >
+                <span class="whitespace-nowrap">{{ n.title }}</span>
+                <i v-if="i == 2" class="fa-solid fa-angle-down ml-1" />
+              </nuxt-link>
+              <div
+                v-if="i == 2"
+                class="hidden group-hover:block absolute right-0 top-full border py-2 text-xs bg-white shadow w-56"
+              >
+                <nuxt-link
+                  :to="`/products/${n.slug}`"
+                  v-for="n in categories"
+                  :class="
+                    $route.params.cat == n.slug
+                      ? 'text-primary font-bold'
+                      : 'hover:bg-secondary'
+                  "
+                  class="px-4 py-2 text-sm flex uppercase font-medium"
+                >
+                  {{ n.name }}
+                </nuxt-link>
+              </div>
+            </div>
             <nuxt-link
               to="/contact"
               class="center h-12 px-4 bg-primary hover:bg-primary text-white font-bold"
@@ -66,29 +88,62 @@
           </button>
         </div>
       </section>
-      <aside
-        v-if="dialog"
-        @click="dialog = false"
-        class="border-b-2 border-b-primary"
-      >
-        <nuxt-link
-          v-for="n in links"
-          :to="$nav({ name: n.name })"
-          class="flex p-4 border-b border-b-primary/20 text-base"
-          :class="
-            route.name.startsWith(n.name)
-              ? 'bg-primary/15 text-primary font-bold'
-              : 'font-medium'
-          "
-        >
-          {{ n.title }}
-        </nuxt-link>
+      <aside v-if="dialog" class="border-b-2 border-b-primary">
+        <div v-for="(n, i) in links" class="relative group">
+          <nuxt-link
+            v-if="i != 2"
+            :to="$nav({ name: n.name })"
+            class="flex p-4 border-b border-b-primary/20 text-base"
+            :class="
+              route.name.startsWith(n.name)
+                ? 'bg-primary/15 text-primary font-bold'
+                : 'font-medium'
+            "
+          >
+            {{ n.title }}
+          </nuxt-link>
+          <div
+            v-else
+            @click="open = !open"
+            class="flex p-4 border-b border-b-primary/20 text-base"
+            :class="
+              route.name.startsWith(n.name) || open
+                ? 'bg-primary/15 text-primary font-bold'
+                : 'font-medium'
+            "
+          >
+            <span class="whitespace-nowrap flex-1">{{ n.title }}</span>
+            <i
+              v-if="i == 2"
+              :class="`fa-solid fa-${open ? 'minus' : 'plus'} ml-1`"
+            />
+          </div>
+          <div
+            v-if="i == 2 && (open || categories.map(el=>el.slug).includes($route.params.cat as string) )"
+            class="py-1 text-xs border-b"
+          >
+            <nuxt-link
+              :to="`/products/${n.slug}`"
+              v-for="n in categories"
+              :class="
+                $route.params.cat == n.slug
+                  ? 'text-primary font-bold'
+                  : 'hover:bg-secondary'
+              "
+              class="px-8 py-3 text-sm flex uppercase"
+            >
+              {{ n.name }}
+            </nuxt-link>
+          </div>
+        </div>
       </aside>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import categories from "@/resources/categories.json";
+const open = ref(false);
 const dialog = ref(false);
 const route: any = useRoute();
 const links = [
@@ -98,6 +153,7 @@ const links = [
   { title: "Terms & Policy", name: "terms" },
   { title: "Contact", name: "contact" },
 ];
+watch(useRoute(), () => ((dialog.value = false), (open.value = false)));
 </script>
 
 <style scoped></style>
